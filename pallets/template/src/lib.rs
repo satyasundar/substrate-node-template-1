6a8ccdc460cd64dc7ffa7cebd0309f82c434f2cc
satyasundar <satyasundar.nayak@gmail.com>
2024-04-14 06:59:38 +0530

    add custom pallet

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,108 +1,81 @@
+//All pallets must be configred for no_std
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/reference/frame-pallets/>
+//Re-export pallet items so that they can be accessed from the crate namespace
 pub use pallet::*;
-
-#[cfg(test)]
-mod mock;
-
-#[cfg(test)]
-mod tests;
-
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
-pub mod weights;
-pub use weights::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		// Because this pallet emits events, it depends on runtime's defination of an event
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-		/// Type representing the weight of this pallet
-		type WeightInfo: WeightInfo;
+		//pallets use weights to measure the complexity of the callable functions
+		type WeightInfo;
 	}
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/main-docs/build/runtime-storage/
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
-
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/main-docs/build/events-errors/
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored { something: u32, who: T::AccountId },
+		// event emitted when the claim is created
+		ClaimCreated { who: T::AccountId, claim: T::Hash },
+		// event emitted when the claim is revoked
+		ClaimRevoked { who: T::AccountId, claim: T::Hash },
 	}
 
-	// Errors inform users that something went wrong.
+
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		AlreadyClaimed,
+		NoSuchClaim,
+		NotClaimOwner,
 	}
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+
+	#[pallet::storage]
+	pub(super) type Claims<T: Config> = StorageMap<_, Blake2_128Concat, T::Hash, (T::AccountId, BlockNumberFor<T>)>;
+
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[pallet::weight(Weight::default())]
 		#[pallet::call_index(0)]
-		#[pallet::weight(T::WeightInfo::do_something())]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/main-docs/build/origins/
-			let who = ensure_signed(origin)?;
+		pub fn create_claim(origin: OriginFor<T>, claim: T::Hash) -> DispatchResult {
+			//check if the extrinsic or transaction is signed
+			let sender = ensure_signed(origin)?;
 
-			// Update storage.
-			<Something<T>>::put(something);
+			//verify that specified claim is not yet stored
+			ensure!(!Claims::<T>::contains_key(&claim), Error::<T>::AlreadyClaimed);
 
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored { something, who });
-			// Return a successful DispatchResultWithPostInfo
+			let current_block = <frame_system::Pallet<T>>::block_number();
+			Claims::<T>::insert(&claim, (&sender, current_block));
+			Self::deposit_event(Event::ClaimCreated { who: sender, claim });
+
 			Ok(())
 		}
 
-		/// An example dispatchable that may throw a custom error.
+		#[pallet::weight(Weight::default())]
 		#[pallet::call_index(1)]
-		#[pallet::weight(T::WeightInfo::cause_error())]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+		pub fn revoke_claim(origin: OriginFor<T>, claim: T::Hash) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let (owner, _) = Claims::<T>::get(&claim).ok_or(Error::<T>::NoSuchClaim)?;
+			ensure!(sender == owner, Error::<T>::NotClaimOwner);
+			Claims::<T>::remove(&claim);
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => return Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
+			Self::deposit_event(Event::ClaimRevoked { who: sender, claim });
+			Ok(())
 		}
 	}
+
 }
+
+pub mod weights {
+	//placeholder for pallet weights
+	pub struct SubstrateWeight<T>(core::marker::PhantomData<T>);
+}
+
